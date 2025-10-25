4d1e74b350eb964b8238a3d14a7087a5b28caac2
dffrs <dffrs@icloud.com>
2025-10-25 19:11:17 +0100

    refactor: remove boilerplate code

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div>Hello there</div>;
+  const [users, setUsers] = useState<any>(() => []);
+
+  return (
+    <div>
+      <button
+        onClick={async () => {
+          const resp = fetch("http://localhost:8080/api/v1/users", {
+            method: "GET",
+          });
+          const users = (await resp).json();
+
+          setUsers(users);
+
+          console.log(users);
+        }}
+      >
+        Get all users
+        {users}
+      </button>
+    </div>
+  );
 }
 
 export default App;
