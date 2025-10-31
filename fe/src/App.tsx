3c568c6d8ca210d4ffc98a19afbeef8d21eb5a0c
dffrs <dffrs@icloud.com>
2025-10-31 20:39:00 +0000

    refactor: Navbar

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,6 +1,4 @@
-import { IoSettingsOutline } from "react-icons/io5";
-import { RiMoneyEuroCircleLine } from "react-icons/ri";
-import { Icon } from "./components";
+import { Navbar } from "./components";
 
 type Users = {
   id: number;
@@ -12,23 +10,9 @@ type Users = {
 
 function App() {
   return (
-    <div>
-      <nav className="flex items-center p-2 text-contrast px-3">
-        <div className="flex flex-row gap-x-2 items-center">
-          <Icon id="icon" title="Purchase Tracker">
-            <RiMoneyEuroCircleLine />
-          </Icon>
-          <label htmlFor="icon" className="text-xs">
-            Purchase Tracker
-          </label>
-        </div>
-        <div className="flex flex-row gap-x-2 ml-auto">
-          <Icon title="Settings">
-            <IoSettingsOutline />
-          </Icon>
-        </div>
-      </nav>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
--- fe/src/components/nav.tsx
+++ fe/src/components/nav.tsx
@@ -1,9 +1,25 @@
-import { FunctionComponent, PropsWithChildren } from "react";
+import { FunctionComponent } from "react";
+import { IoSettingsOutline } from "react-icons/io5";
+import { RiMoneyEuroCircleLine } from "react-icons/ri";
 
-export const Navbar: FunctionComponent<PropsWithChildren> = ({ children }) => {
+import { Icon } from "./icon";
+
+export const Navbar: FunctionComponent = () => {
   return (
-    <nav className="grid grid-flow-col items-center justify-around gap-x-2 bg-white text-black min-h-10 max-h-12">
-      {children}
+    <nav className="flex items-center p-2 text-contrast px-3">
+      <div className="flex flex-row gap-x-2 items-center">
+        <Icon id="icon" title="Purchase Tracker">
+          <RiMoneyEuroCircleLine />
+        </Icon>
+        <label htmlFor="icon" className="text-xs">
+          Purchase Tracker
+        </label>
+      </div>
+      <div className="flex flex-row gap-x-2 ml-auto">
+        <Icon title="Settings">
+          <IoSettingsOutline />
+        </Icon>
+      </div>
     </nav>
   );
 };
