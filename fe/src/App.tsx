d8e23fbc9d317463fad947ef0893591a69833490
Daniel Rodrigues <daniel.rodrigues@nokia.com>
2025-11-05 16:06:54 +0000

    fix: sometimes clicking on nav buttons would not register

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -19,22 +19,22 @@ function App() {
           <aside className="bg-contrast text-contrast p-2">
             <ul className="grid grid-flow-row gap-y-4">
               <li className="flex flex-row gap-x-2 items-center">
-                <Button className="bg-secondary rounded p-3">
-                  <Link to="/home">
+                <Link to="/home">
+                  <Button className="bg-secondary rounded p-3">
                     <Icon title="Home" className="text-xl">
                       <IoHome />
                     </Icon>
-                  </Link>
-                </Button>
+                  </Button>
+                </Link>
               </li>
               <li className="flex flex-row gap-x-2 items-center">
-                <Button className="bg-secondary rounded p-3">
-                  <Link to="/search">
+                <Link to="/search">
+                  <Button className="bg-secondary rounded p-3">
                     <Icon title="Search" className="text-xl">
                       <IoSearch />
                     </Icon>
-                  </Link>
-                </Button>
+                  </Button>
+                </Link>
               </li>
             </ul>
           </aside>
