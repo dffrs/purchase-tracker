2149f3c4550e4fe7b1236fb81084f6d3da77003c
dffrs <dffrs@icloud.com>
2025-11-08 15:03:52 +0000

    fix: home cards width

--- fe/src/pages/home.tsx
+++ fe/src/pages/home.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent } from "react";
 export const Home: FunctionComponent = () => {
   return (
     <div className="h-full w-full grid grid-flow-row grid-rows-[auto,1fr]">
-      <ul className="grid grid-flow-col gap-x-8 h-96">
+      <ul className="grid grid-cols-2 gap-x-8 h-96">
         <li className="rounded-xl bg-secondary shadow-xl">
           <span className="flex flex-col gap-y-4 justify-center items-start h-full p-8">
             <h1 className="font-bold text-2xl text-contrast">Orders</h1>
