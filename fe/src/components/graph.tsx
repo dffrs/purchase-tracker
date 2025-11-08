d067371c6ac8a979fd7e3d1067f7901e06cd498c
dffrs <dffrs@iscte-iul.pt>
2025-11-08 16:30:51 +0000

    refactor: graph layout [wip]

--- fe/src/components/graph.tsx
+++ fe/src/components/graph.tsx
@@ -10,31 +10,19 @@ const mockData = [
 
 export const Graph: FunctionComponent<GraphProps> = ({ data = mockData }) => {
   const max = Math.max(...data);
-  const min = Math.min(...data);
-
-  // for (i = 0; i < data.length; i++) {
-  //   h = Math.round(100 * ((data[i] - min) / max));
-  //   html +=
-  //     '<div class="bar" style="height:' +
-  //     h +
-  //     "%; left:" +
-  //     12 * i +
-  //     'px">' +
-  //     data[i] +
-  //     "</div>";
-  // }
+  const min = Math.min(...data) * 0.1;
 
   return (
-    <div className="bg-pop relative h-full w-full table-cell align-bottom">
+    <div className="bg-pop h-full w-full flex items-end gap-x-4">
       {data.map((value, i) => {
         const h = Math.round(100 * ((value - min) / max));
+
         return (
           <div
-            className="absolute bottom-0 inline-block w-[10px] m-0 bg-contrast"
-            style={{ height: h + "%", left: data.length * i + "px" }}
-          >
-            {value}
-          </div>
+            className="w-28 bg-contrast rounded-t-xl"
+            style={{ height: `${h}%`, left: `${data.length * i}px` }}
+            title={String(value)}
+          />
         );
       })}
     </div>
