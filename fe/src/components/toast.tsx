aebbf47d7bec6eb59aceaca5e1fbfad9dfc0c7f1
Daniel Rodrigues <daniel.rodrigues@nokia.com>
2025-11-18 15:08:57 +0000

    feat: Toasts can be closed now

--- fe/src/components/toast.tsx
+++ fe/src/components/toast.tsx
@@ -2,9 +2,13 @@ import {
   createContext,
   FunctionComponent,
   PropsWithChildren,
+  useCallback,
   useContext,
   useState,
 } from "react";
+import { Button } from "./button";
+import { Icon } from "./icon";
+import { IoClose } from "react-icons/io5";
 
 type ToastCtx = {
   createToast: () => void;
@@ -19,21 +23,58 @@ export const useToast = () => {
   return ctx;
 };
 
+type ToastProps = {
+  id: number;
+  onClose: React.MouseEventHandler<HTMLButtonElement>;
+};
+
+const Toast: FunctionComponent<PropsWithChildren<ToastProps>> = ({
+  id,
+  onClose,
+  children,
+}) => {
+  return (
+    <span className="bg-contrast p-2 rounded-xl shadow-xl flex flex items-center gap-x-2">
+      <Button
+        id={String(id)}
+        className="shadow-none rounded-full p-0"
+        onClick={onClose}
+      >
+        <Icon title="Close" className="text-base">
+          <IoClose />
+        </Icon>
+      </Button>
+      {children}
+    </span>
+  );
+};
+
 export const ToastProvider: FunctionComponent<PropsWithChildren> = ({
   children,
 }) => {
-  const [toasts, setToasts] = useState<any[]>(() => []);
+  const [toasts, setToasts] = useState<number[]>(() => []);
 
   const createToast = () => {
     setToasts((prev) => [...prev, (prev.at(-1) ?? 0) + 1]);
   };
+
+  const onClose: ToastProps["onClose"] = useCallback((event) => {
+    const toastId = event.currentTarget.id;
+    setToasts((prev) => prev.filter((t) => t !== +toastId));
+  }, []);
 
   return (
     <ToastContext.Provider value={{ createToast }}>
       {children}
       <ul className="absolute bottom-10 left-[50%] max-h-80 p-2 flex flex-col gap-y-4">
         {toasts?.map((toast, i) => {
-          return <li className="bg-contrast">toast {i + 1}</li>;
+          return (
+            <li key={i}>
+              <Toast id={toast} onClose={onClose}>
+                toast {i + 1}
+              </Toast>
+            </li>
+          );
         })}
       </ul>
     </ToastContext.Provider>
