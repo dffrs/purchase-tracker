45e485baa5b386e96e156d4e669f7860c9b58847
dffrs <dffrs@icloud.com>
2025-11-22 12:12:39 +0000

    feat: created city schema

---
+++ be/cmd/migrate/migrations/000006_create_city_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS city (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  zip_code TEXT NOT NULL,
+  country_id INTEGER NOT NULL,
+  FOREIGN KEY (country_id) REFERENCES country(id)
+  -- UNIQUE(name, zip_code, country_id) TODO: 
+)
