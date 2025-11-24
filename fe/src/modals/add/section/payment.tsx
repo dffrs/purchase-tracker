afef8bce2977f2d9ea67a2595adfc40ffe4aa472
dffrs <dffrs@iscte-iul.pt>
2025-11-24 21:59:04 +0000

    feat: changed color of labels (for radio inputs)

--- fe/src/modals/add/section/payment.tsx
+++ fe/src/modals/add/section/payment.tsx
@@ -3,14 +3,14 @@ import { FunctionComponent } from "react";
 
 export const PaymentMethodSection: FunctionComponent = () => {
   return (
-    <div className="flex items-center gap-x-2 text-contrast">
+    <div className="flex items-center gap-x-2">
       <Input
         type="radio"
         name="payment"
         id="mbway"
         label="MBWay"
         className="accent-contrast cursor-pointer"
-        labelClassName="!flex-row-reverse gap-x-2 cursor-pointer"
+        labelClassName="!flex-row-reverse gap-x-2 cursor-pointer text-pop"
       />
       <Input
         type="radio"
@@ -18,7 +18,7 @@ export const PaymentMethodSection: FunctionComponent = () => {
         id="cash"
         label="Cash"
         className="accent-contrast cursor-pointer"
-        labelClassName="!flex-row-reverse gap-x-2 cursor-pointer"
+        labelClassName="!flex-row-reverse gap-x-2 cursor-pointer text-pop"
       />
       <Input
         type="radio"
@@ -26,7 +26,7 @@ export const PaymentMethodSection: FunctionComponent = () => {
         id="card"
         label="Card"
         className="accent-contrast cursor-pointer"
-        labelClassName="!flex-row-reverse gap-x-2 cursor-pointer"
+        labelClassName="!flex-row-reverse gap-x-2 cursor-pointer text-pop"
       />
       <Input
         type="radio"
@@ -34,7 +34,7 @@ export const PaymentMethodSection: FunctionComponent = () => {
         id="paypal"
         label="Paypal"
         className="accent-contrast cursor-pointer"
-        labelClassName="!flex-row-reverse gap-x-2 cursor-pointer"
+        labelClassName="!flex-row-reverse gap-x-2 cursor-pointer text-pop"
       />
       <Input
         type="radio"
@@ -42,7 +42,7 @@ export const PaymentMethodSection: FunctionComponent = () => {
         id="other"
         label="Other"
         className="accent-contrast cursor-pointer"
-        labelClassName="!flex-row-reverse gap-x-2 cursor-pointer"
+        labelClassName="!flex-row-reverse gap-x-2 cursor-pointer text-pop"
       />
     </div>
   );
