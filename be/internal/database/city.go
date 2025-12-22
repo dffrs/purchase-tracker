f0830ecbebc67d08a061d32160ac3baa5757b3ca
Daniel Rodrigues <daniel.rodrigues@nokia.com>
2025-12-22 15:53:26 +0000

    feat: added Insert method to City struct

--- be/internal/database/city.go
+++ be/internal/database/city.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	"context"
 	"database/sql"
+	"time"
 )
 
 type CityModel struct {
@@ -13,4 +15,26 @@ type City struct {
 	Name      *string
 	ZipCode   *string
 	CountryID *int
+}
+
+func (u *CityModel) Insert(city *City) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := "INSERT INTO city (name, zip_code, country_id) VALUES ($1, $2, $3)"
+
+	result, err := u.DB.ExecContext(ctx, query, city.Name, city.ZipCode, city.CountryID)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return nil
+	}
+
+	castID := int(id)
+	city.ID = &castID
+
+	return nil
 }
