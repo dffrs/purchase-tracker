005867a28d042e949a960bc51ad092c1c727a527
Daniel Rodrigues <daniel.rodrigues@nokia.com>
2025-12-22 15:50:24 +0000

    feat: added Insert method to Address struct

--- be/internal/database/address.go
+++ be/internal/database/address.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	"context"
 	"database/sql"
+	"time"
 )
 
 type AddressModel struct {
@@ -14,4 +16,26 @@ type Address struct {
 	StreetNumber *string
 	Apartment    *string
 	CityID       *int
+}
+
+func (u *AddressModel) Insert(address *Address) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := "INSERT INTO address (street, street_number, apartment, city_id) VALUES ($1, $2, $3, $4)"
+
+	result, err := u.DB.ExecContext(ctx, query, address.Street, address.StreetNumber, address.Apartment, address.CityID, time.Now().Unix())
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return nil
+	}
+
+	castID := int(id)
+	address.ID = &castID
+
+	return nil
 }
