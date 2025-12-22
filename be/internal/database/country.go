d20513b549d99e682a091228a0b3d988d280d8a4
Daniel Rodrigues <daniel.rodrigues@nokia.com>
2025-12-22 16:03:55 +0000

    feat: added Insert method to Country struct

--- be/internal/database/country.go
+++ be/internal/database/country.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	"context"
 	"database/sql"
+	"time"
 )
 
 type CountryModel struct {
@@ -12,4 +14,26 @@ type Country struct {
 	ID   *int
 	Code *string
 	Name *string
+}
+
+func (u *CountryModel) Insert(country *Country) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := "INSERT INTO city (code, name) VALUES ($1, $2)"
+
+	result, err := u.DB.ExecContext(ctx, query, country.Code, country.Name)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return nil
+	}
+
+	castID := int(id)
+	country.ID = &castID
+
+	return nil
 }
