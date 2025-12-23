1b9e4bba5a37be62dab64e85dcb93b0acc467e0f
Daniel Rodrigues <daniel.rodrigues@nokia.com>
2025-12-23 12:15:15 +0000

    refactor: removed all old controller methods (order items)

--- be/cmd/api/routes.go
+++ be/cmd/api/routes.go
@@ -33,12 +33,6 @@ func (app *application) routes() http.Handler {
 
 		// order items
 		v1.POST("/orderItems", app.createOrderItems)
-		v1.GET("/orderItems/:order_item_id", app.getOrderItems)
-		v1.GET("/orderItems/order/:order_id", app.getOrderItemsByOrderID)
-		v1.GET("/orderItems/product/:product_id", app.getOrderItemsByProductID)
-		v1.GET("/orderItems/user/id/:user_id", app.getOrderItemsByUserID)
-		v1.GET("/orderItems/user/email/:user_email", app.getOrderItemsByUserEmail)
-		v1.GET("/orderItems/user/phone/:user_phone", app.getOrderItemsByUserPhone)
 	}
 
 	return g
