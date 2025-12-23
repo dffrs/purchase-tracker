4347e1ff2ae0aed269644943cffd2409c70a0b38
Daniel Rodrigues <daniel.rodrigues@nokia.com>
2025-12-23 12:33:52 +0000

    feat: created GetOrInsert method in Payment struct

--- be/internal/database/payment.go
+++ be/internal/database/payment.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	"context"
 	"database/sql"
+	"time"
 )
 
 type PaymentModel struct {
@@ -11,4 +13,26 @@ type PaymentModel struct {
 type Payment struct {
 	ID   *int
 	Name *string
+}
+
+func (p *PaymentModel) GetOrCreate(payment *string) (*string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	insert := "INSERT OR IGNORE INTO payment (name) VALUES (?)"
+
+	_, err := p.DB.ExecContext(ctx, insert, payment)
+	if err != nil {
+		return nil, err
+	}
+
+	query := "SELECT id FROM payment WHERE id = ?"
+
+	var pName string
+	err = p.DB.QueryRowContext(ctx, query, payment).Scan(&pName)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pName, nil
 }
