374e78b7c667a98fddb379e45869a93c9dec0063
Daniel Rodrigues <daniel.rodrigues@nokia.com>
2025-12-23 12:13:36 +0000

    feat: created OrderPayload struct (be)

--- be/internal/models/models.go
+++ be/internal/models/models.go
@@ -30,3 +30,18 @@ type UserResponse struct {
 	CreatedAt time.Time       `json:"createdAt"`
 	Address   AddressResponse `json:"address"`
 }
+
+type Product struct {
+	Name     string  `json:"name" binding:"required"`
+	Code     string  `json:"code" binding:"required"`
+	Quantity int     `json:"quantity" binding:"required"`
+	RRP      float64 `json:"rrp" binding:"required"`
+	WSP      float64 `json:"wsp" binding:"required"`
+}
+
+type OrderPayload struct {
+	User     UserResponse `json:"user" binding:"required"`
+	Payment  string       `json:"payment" binding:"required"`
+	Delivery string       `json:"delivery" binding:"required"`
+	Products []Product    `json:"products" binding:"required"`
+}
