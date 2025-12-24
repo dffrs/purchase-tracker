9518b6bda80e264935cfc976a4f8d4b37b2351fe
dffrs <dffrs@icloud.com>
2025-12-24 11:37:03 +0000

    feat: table styles

--- fe/src/pages/search.tsx
+++ fe/src/pages/search.tsx
@@ -27,25 +27,20 @@ export const Search: FunctionComponent = () => {
     <div className="card h-full w-full p-8 grid grid-flow-row grid-rows-[auto,1fr] gap-y-8">
       <h1 className="card-header">Orders</h1>
       <LoadingArea isLoading={isLoading}>
-        <table className="w-full border-collapse text-left">
-          <thead className="text-contrast">
+        <table>
+          <thead>
             <tr>
               {columns.map(({ title, key }) => (
-                <th className="border-2 border-solid border-pop px-6" key={key}>
-                  {title}
-                </th>
+                <th key={key}>{title}</th>
               ))}
             </tr>
           </thead>
-          <tbody className="text-pop">
+          <tbody>
             {allOrders.map((orders, index) => {
               return (
                 <tr key={`${index}`}>
                   {columns.map(({ key }) => (
-                    <td
-                      className="border-2 border-solid border-pop px-6"
-                      key={`${key}-${index}-${orders[key]}`}
-                    >
+                    <td key={`${key}-${index}-${orders[key]}`}>
                       {orders[key]}
                     </td>
                   ))}
