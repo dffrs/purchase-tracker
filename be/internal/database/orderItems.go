3dc59e553c6002b24691a7dda7f718cfdb9799b7
Daniel Rodrigues <daniel.rodrigues@nokia.com>
2025-12-29 16:20:14 +0000

    fix: removed dead methods (OrderItemsModel struct)

--- be/internal/database/orderItems.go
+++ be/internal/database/orderItems.go
@@ -3,7 +3,6 @@ package database
 import (
 	"context"
 	"database/sql"
-	"fmt"
 	"time"
 )
 
@@ -45,69 +44,6 @@ type OrdersSearch struct {
 	RRPAtPurchase float64 `json:"rrpAtPurchase"`
 	WSPAtPurchase float64 `json:"wspAtPurchase"`
 	Quantity      int     `json:"quantity"`
-}
-
-func getByUserProps[T string | int](oi *OrderItemsModel, dbColumn string, value T) ([]*OrdersResponse, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
-	query := fmt.Sprintf(` 
-	SELECT 
-		users.name AS name,
-		users.email AS email,
-		users.phone AS phone,
-		products.name AS productName,
-		products.code AS productCode,
-		products.rrp AS productRRP,
-		products.wsp AS productWSP,
-		orders.order_date AS orderDate,
-		order_items.quantity AS quantity,
-		order_items.rrp_at_purchase AS rrpAtPurchase,
-		order_items.wsp_at_purchase AS wspAtPurchase
-	FROM
-		users
-		INNER JOIN orders ON orders.user_id = users.id
-		INNER JOIN order_items ON order_items.order_id = orders.id
-		INNER JOIN products ON products.id = order_items.product_id
-	WHERE
-		users.%s = $1
-	`, dbColumn)
-
-	rows, err := oi.DB.QueryContext(ctx, query, value)
-	if err != nil {
-		return nil, err
-	}
-
-	orderUsers := []*OrdersResponse{}
-
-	for rows.Next() {
-		orderUser := new(OrdersResponse)
-
-		err := rows.Scan(
-			&orderUser.Name,
-			&orderUser.Email,
-			&orderUser.Phone,
-			&orderUser.ProductName,
-			&orderUser.ProductCode,
-			&orderUser.ProductRRP,
-			&orderUser.ProductWSP,
-			&orderUser.OrderDate,
-			&orderUser.Quantity,
-			&orderUser.RRPAtPurchase,
-			&orderUser.WSPAtPurchase,
-		)
-		if err != nil {
-			return nil, err
-		}
-
-		orderUsers = append(orderUsers, orderUser)
-	}
-
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return orderUsers, nil
 }
 
 func (oi *OrderItemsModel) Insert(orderItem *OrdersItem) error {
@@ -263,82 +199,4 @@ func (oi OrderItemsModel) FindBy(search string) ([]*OrdersSearch, error) {
 	}
 
 	return ordersSearch, nil
-}
-
-// NOTE: do I need this ? If so, update to get product.rrp & product.wsp
-
-func (oi *OrderItemsModel) GetByOrderID(orderID int) ([]*OrdersItem, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
-	query := "SELECT order_items.id, order_items.order_id, order_items.product_id, order_items.quantity, order_items.rrp_at_purchase, order_items.wsp_at_purchase FROM order_items JOIN orders ON orders.id = order_items.order_id WHERE orders.id = $1"
-
-	rows, err := oi.DB.QueryContext(ctx, query, orderID)
-	if err != nil {
-		return nil, err
-	}
-
-	orderItems := []*OrdersItem{}
-
-	for rows.Next() {
-		orderItem := new(OrdersItem)
-
-		err := rows.Scan(&orderItem.ID, &orderItem.OrderID, &orderItem.ProductID, &orderItem.Quantity, &orderItem.RRPAtPurchase, &orderItem.WSPAtPurchase)
-		if err != nil {
-			return nil, err
-		}
-
-		orderItems = append(orderItems, orderItem)
-	}
-
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return orderItems, nil
-}
-
-// NOTE: do I need this ? If so, update to get product.rrp & product.wsp
-
-func (oi *OrderItemsModel) GetByProductID(productID int) ([]*OrdersItem, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
-	query := "SELECT order_items.id, order_items.order_id, order_items.product_id, order_items.quantity, order_items.rrp_at_purchase, order_items.wsp_at_purchase FROM order_items JOIN products ON products.id = order_items.product_id WHERE products.id = $1"
-
-	rows, err := oi.DB.QueryContext(ctx, query, productID)
-	if err != nil {
-		return nil, err
-	}
-
-	orderItems := []*OrdersItem{}
-
-	for rows.Next() {
-		orderItem := new(OrdersItem)
-
-		err := rows.Scan(&orderItem.ID, &orderItem.OrderID, &orderItem.ProductID, &orderItem.Quantity, &orderItem.RRPAtPurchase, &orderItem.WSPAtPurchase)
-		if err != nil {
-			return nil, err
-		}
-
-		orderItems = append(orderItems, orderItem)
-	}
-
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return orderItems, nil
-}
-
-func (oi OrderItemsModel) GetByUserID(userID int) ([]*OrdersResponse, error) {
-	return getByUserProps(&oi, "id", userID)
-}
-
-func (oi OrderItemsModel) GetByUserEmail(userEmail string) ([]*OrdersResponse, error) {
-	return getByUserProps(&oi, "email", userEmail)
-}
-
-func (oi OrderItemsModel) GetByUserPhone(userPhone int) ([]*OrdersResponse, error) {
-	return getByUserProps(&oi, "phone", userPhone)
 }
