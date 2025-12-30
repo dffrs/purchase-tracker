5dc1d5bc3459f45c6bd14613b1fcb2fa4681f7c6
Daniel Rodrigues <daniel.rodrigues@nokia.com>
2025-12-30 11:43:36 +0000

    feat: render real 'count' & 'profit' in homepage

--- fe/src/hooks/index.ts
+++ fe/src/hooks/index.ts
@@ -1,2 +1,4 @@
 export { useGetAllUsers } from "./useGetAllUsers";
 export { useGetAllProducts } from "./useGetAllProducts";
+export { useGetAllOrders } from "./useGetAllOrders";
+export { useGetAllOrderStats } from "./useGetAllOrderStats";
--- fe/src/pages/home.tsx
+++ fe/src/pages/home.tsx
@@ -1,33 +1,46 @@
-import { Graph } from "@/components";
-import { FunctionComponent } from "react";
+import { Graph, useToast } from "@/components";
+import { LoadingArea } from "@/components/loadingArea";
+import { useGetAllOrderStats } from "@/hooks";
+import { FunctionComponent, useEffect } from "react";
 
 const mockData = [
   34.7, 68.9, 65.1, 130.2, 208.6, 172.8, 155.0, 168.6, 134.4, 52.7, 94.5, 41.5,
 ];
 
 export const Home: FunctionComponent = () => {
+  const [stats, isLoading, error] = useGetAllOrderStats();
+  const createToast = useToast();
+
+  useEffect(() => {
+    if (error != null) createToast("Failed to get stats");
+  }, [error]);
+
   return (
     <div className="h-full w-full grid grid-flow-row grid-rows-[auto,1fr] gap-y-8">
       <ul className="grid grid-cols-2 gap-x-8 h-96">
         <li className="card overflow-hidden">
           <span className="card-layout">
             <h1 className="card-header">Orders</h1>
-            <p className="card-text animate-fadeAndMoveIn">
-              {(42).toLocaleString("en", { style: "decimal" })}
-            </p>
+            <LoadingArea isLoading={isLoading}>
+              <p className="card-text animate-fadeAndMoveIn">
+                {stats.count.toLocaleString("en", { style: "decimal" })}
+              </p>
+            </LoadingArea>
           </span>
         </li>
         <li className="card overflow-hidden">
           <span className="card-layout">
             <h1 className="card-header">Profit</h1>
-            <p className="card-text animate-fadeAndMoveIn">
-              {(134.43).toLocaleString("en", {
-                style: "currency",
-                currency: "EUR",
-                maximumFractionDigits: 2,
-                trailingZeroDisplay: "stripIfInteger",
-              })}
-            </p>
+            <LoadingArea isLoading={isLoading}>
+              <p className="card-text animate-fadeAndMoveIn">
+                {stats.profit.toLocaleString("en", {
+                  style: "currency",
+                  currency: "EUR",
+                  maximumFractionDigits: 2,
+                  trailingZeroDisplay: "stripIfInteger",
+                })}
+              </p>
+            </LoadingArea>
           </span>
         </li>
       </ul>
