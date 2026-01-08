8dfc9fae495270ec0c2dadf399bd5b2991da4f80
Daniel Rodrigues <daniel.rodrigues@nokia.com>
2026-01-08 15:29:00 +0000

    fix: GetOrCreate User won't use email to find user

--- be/internal/database/users.go
+++ be/internal/database/users.go
@@ -80,10 +80,10 @@ func (u *UsersModel) GetOrCreate(name, email *string, phone, addressID *int) (in
 		return 0, err
 	}
 
-	query := "SELECT id FROM users WHERE name = ? AND email = ? AND phone = ?"
+	query := "SELECT id FROM users WHERE name = ? AND phone = ?"
 
 	var id int
-	err = u.DB.QueryRowContext(ctx, query, name, email, phone).Scan(&id)
+	err = u.DB.QueryRowContext(ctx, query, name, phone).Scan(&id)
 	if err != nil {
 		return 0, err
 	}
